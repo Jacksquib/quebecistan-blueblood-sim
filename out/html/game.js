0be656cc7766eee966e67bf935d4d2f32cb71c58
Jacksquib <83320035+Jacksquib@users.noreply.github.com>
2025-10-28 06:38:51 +1030

    e

---
+++ out/html/game.js
@@ -0,0 +1,121 @@
+(function() {
+  var game;
+  var ui;
+
+  var DateOptions = {hour: 'numeric',
+                 minute: 'numeric',
+                 second: 'numeric',
+                 year: 'numeric',
+                 month: 'short',
+                 day: 'numeric' };
+
+  var main = function(dendryUI) {
+    ui = dendryUI;
+    game = ui.game;
+
+    // Add your custom code here.
+  };
+
+  var TITLE = "Québecistan Blueblood Sim" + '_' + "Jacksquib";
+
+  window.showStats = function() {
+    if (window.dendryUI.dendryEngine.state.sceneId.startsWith('library')) {
+        window.dendryUI.dendryEngine.goToScene('backSpecialScene');
+    } else {
+        window.dendryUI.dendryEngine.goToScene('library');
+    }
+  };
+
+  // This function allows you to modify the text before it's displayed.
+  // E.g. wrapping chat-like messages in spans.
+  window.displayText = function(text) {
+      return text;
+  };
+
+  // This function allows you to do something in response to signals.
+  window.handleSignal = function(signal, event, scene_id) {
+  };
+  
+  // This function runs on a new page. Right now, this auto-saves.
+  window.onNewPage = function() {
+    var scene = window.dendryUI.dendryEngine.state.sceneId;
+    if (scene != 'root' && !window.justLoaded) {
+        window.dendryUI.autosave();
+    }
+    if (window.justLoaded) {
+        window.justLoaded = false;
+    }
+  };
+
+  // tabbed browsing
+  window.updateSidebar = function() {
+      $('#qualities').empty();
+      var scene = dendryUI.game.scenes[window.statusTab];
+      dendryUI.dendryEngine._runActions(scene.onArrival);
+      var displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
+      $('#qualities').append(dendryUI.contentToHTML.convert(displayContent));
+  };
+
+  window.changeTab = function(newTab, tabId) {
+      var tabButton = document.getElementById(tabId);
+      var tabButtons = document.getElementsByClassName('tab_button');
+      for (i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].className = tabButtons[i].className.replace(' active', '');
+      }
+      tabButton.className += ' active';
+      window.statusTab = newTab;
+      window.updateSidebar();
+  };
+
+
+
+  window.onDisplayContent = function() {
+      window.updateSidebar();
+  };
+
+  /*
+   * This function copied from the code for Infinite Space Battle Simulator
+   *
+   * quality - a number between max and min
+   * qualityName - the name of the quality
+   * max and min - numbers
+   * colors - if true/1, will use some color scheme - green to yellow to red for high to low
+   * */
+  window.generateBar = function(quality, qualityName, max, min, colors) {
+      var bar = document.createElement('div');
+      bar.className = 'bar';
+      var value = document.createElement('div');
+      value.className = 'barValue';
+      var width = (quality - min)/(max - min);
+      if (width > 1) {
+          width = 1;
+      } else if (width < 0) {
+          width = 0;
+      }
+      value.style.width = Math.round(width*100) + '%';
+      if (colors) {
+          value.style.backgroundColor = window.probToColor(width*100);
+      }
+      bar.textContent = qualityName + ': ' + quality;
+      if (colors) {
+          bar.textContent += '/' + max;
+      }
+      bar.appendChild(value);
+      return bar;
+  };
+
+
+  window.justLoaded = true;
+  window.statusTab = "status";
+  window.dendryModifyUI = main;
+  console.log("Modifying stats: see dendryUI.dendryEngine.state.qualities");
+
+  window.onload = function() {
+    window.dendryUI.loadSettings({show_portraits: false});
+    if (window.dendryUI.dark_mode) {
+        document.body.classList.add('dark-mode');
+    }
+    window.pinnedCardsDescription = "Advisor cards - actions are only usable once per 6 months.";
+  };
+
+}());
